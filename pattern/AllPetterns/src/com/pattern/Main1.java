ac3409e542d54ed2baa92b5435ee15ebc6f1c82f
kanhaiyatiwari <tiwarikanhaiya149@gmail.com>
2023-12-06 12:01:38 +0530

    add

---
+++ pattern/AllPetterns/src/com/pattern/Main1.java
@@ -0,0 +1,24 @@
+package com.pattern;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+//		******
+//		*    *
+//		*    *
+//		*    *
+//		*    *
+//		******
+		
+		for(int i=0;i<6;i++) {
+			for(int j=0;j<6;j++) {
+				if(i==0||j==0||i==5||j==5)
+				System.out.print("*");
+				else System.out.print(" ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
